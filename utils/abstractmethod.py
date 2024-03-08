f597f433161bfe498f9998e7480c03824943d2e3
Илья Рожкович <rozhkovich.ilya97@gmail.com>
2024-03-08 21:40:59 +0300

    ‘доработка’

--- utils/abstractmethod.py
+++ utils/abstractmethod.py
@@ -11,27 +11,32 @@ class AbstractProduct(ABC):
 
 
 class ReprMixin:
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__repr__()
+
     def __repr__(self):
         attributes = ', '.join(f"{value}" for value in self.__dict__.values())
         return f"{self.__class__.__name__}({attributes})"
 
 
-class Product(AbstractProduct, ReprMixin):
+class Product(AbstractProduct, ReprMixin, ABC):
     """Класс для продуктов."""
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name, description, price, quantity, **kwargs):
+        super().__init__(**kwargs)
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
+        self.__repr__()
 
-    @staticmethod
-    def create_product(**kwargs):
-        """Метод для создания нового продукта из словаря с данными."""
-        return Product(**kwargs)
+    def create_product(self, **kwargs):
+        """Метод для создания нового продукта."""
+        return self.__class__(**kwargs)
 
 
-class Smartphone(Product):
+class Smartphone(Product, ABC):
     """Класс для смартфонов."""
 
     def __init__(self, name, description, price, quantity, performance, model, storage_capacity, color):
@@ -41,13 +46,8 @@ class Smartphone(Product):
         self.storage_capacity = storage_capacity
         self.color = color
 
-    @staticmethod
-    def create_product(**kwargs):
-        """Метод для создания нового смартфона из словаря с данными."""
-        return Smartphone(**kwargs)
 
-
-class LawnGrass(Product):
+class LawnGrass(Product, ABC):
     """Класс для травы газонной."""
 
     def __init__(self, name, description, price, quantity, country_of_origin, germination_period, color):
@@ -56,19 +56,13 @@ class LawnGrass(Product):
         self.germination_period = germination_period
         self.color = color
 
-    @staticmethod
-    def create_product(**kwargs):
-        """Метод для создания новой травы газонной из словаря с данными."""
-        return LawnGrass(**kwargs)
-
 
 product = Product('Продукт1', 'Описание продукта', 1200, 10)
-print(product)
-
 smartphone = Smartphone("Смартфон", "Мощный смартфон", 1000, 10, "Apple",
                         "iPhone 12", "256 ГБ", "Чёрный")
-print(smartphone)
-
 lawn_grass = LawnGrass("Трава", "Смесь для газонов", 50, 100, "Россия",
                        "30 дней", "Зелёный")
+
+print(product)
+print(smartphone)
 print(lawn_grass)
